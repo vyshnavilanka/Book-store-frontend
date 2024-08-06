11276956c4ce1e491e720a62961c09954758e13a
vyshnavilanka <vaishulanka007@gmail.com>
2024-08-06 20:29:34 +0530

    first commit

---
+++ src/components/DeleteBook.jsx
@@ -0,0 +1,20 @@
+import React,{useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const DeleteBook = () => {
+    const navigate = useNavigate()
+    const {id} = useParams()
+    useEffect(() =>{
+        axios.delete('http://localhost:3001/book/book/'+id)
+        .then(res =>{
+            if(res.data.deleted){
+                navigate('/books')
+
+            }
+        }).catch(err => console.log(err))
+
+    },[])
+}
+
+export default DeleteBook
---
+++ src/components/EditBook.jsx
@@ -0,0 +1,70 @@
+import React ,{ useEffect,useState } from 'react'
+import {useNavigate,useParams} from 'react-router-dom'
+import "../css/Login.css"
+import axios from 'axios'
+
+const EditBook = () => {
+    const[name, setName]= useState('')
+    const[author, setAuthor] = useState('')
+    const[imageUrl, setImageUrl] = useState('')
+    const navigate =useNavigate()
+    const {id} = useParams()
+    useEffect(() =>{
+        axios.get('http://localhost:3001/book/book/'+id)
+        .then (res =>{
+            setName(res.data.name)
+            setAuthor(res.data.author)
+            setImageUrl(res.data.imageUrl)
+            
+         })
+        .catch(err =>console.log(err))
+
+    },[])
+
+
+    const handleSubmit =(e) =>{
+        e.preventDefault()
+        axios.put('http://localhost:3001/book/book/'+id,{name,author,imageUrl})
+        .then (res =>{
+            if(res.data.updated){
+                navigate('/books') 
+            }
+            else{
+                console.log(res)
+            }   
+         })
+        .catch(err =>console.log(err))
+          
+    }
+
+
+  return (
+    <div className ="student-form-container">
+        <form className ="student-form" onSubmit={handleSubmit}>
+            <h2>Edit Book</h2>
+            <div className="form-group">
+                <label htmlFor="book">Book Name:</label>
+                <input type ="text" id ="book" name ="book" value={name}
+                onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="author">Author Name:</label>
+                <input type="text" id ="author" name ="username" value={author}
+                onChange={(e) => setAuthor(e.target.value)}/>
+
+            </div>
+            <div className ="form-group">
+                <label htmlFor="image">Image Url:</label>
+                <input type="text" id ="image" name ="image" value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}/>
+            </div>
+           
+            <button  className ="student-form button"type="submit">Update</button>
+        </form>
+    </div>
+    
+  )
+}
+
+
+export default EditBook
